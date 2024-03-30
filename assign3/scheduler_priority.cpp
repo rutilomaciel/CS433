4519faef7001ae856551ccdc718b96ae20a81c43
rutilomaciel <rutilomaciel@hotmail.com>
2024-03-29 17:27:55 -0700

    Updated priority

--- assign3/scheduler_priority.cpp
+++ assign3/scheduler_priority.cpp
@@ -25,16 +25,68 @@ void SchedulerPriority::init(std::vector<PCB>& process_list) {
     this->process_list = process_list;
 }
 
-void SchedulerPriority::simulate() {
-//    std::sort(this->process_list.begin(), this->process_list.end(), comparePriority);
-//    unsigned int waitTime = 0;
-//
-//    for (PCB& pcb : this->process_list) {
-//        cout << "Running Process " << pcb.name << " for " << pcb.burst_time << " time units | wait time:" << waitTime << endl;
-//        pcb.arrival_time = waitTime;
-//        waitTime += pcb.burst_time;
-//        pcb.burst_time = waitTime;
-//    }
+bool comparePriority(PCB& pcb1, PCB& pcb2) {
+    if (pcb1.priority == pcb2.priority) {
+        // If priorities are equal, compare process numbers in descending order
+        return pcb1.id < pcb2.id;
+    }
+    // Compare priorities in ascending order
+    return pcb1.priority > pcb2.priority;
 }
 
-void SchedulerPriority::print_results() {}
+bool compareId(const PCB& pcb1, const PCB& pcb2) {
+    return pcb1.id < pcb2.id;
+}
+
+void SchedulerPriority::simulate() {
+    std::sort(this->process_list.begin(), this->process_list.end(), comparePriority );
+
+
+    unsigned int  waitTime = 0;
+
+    for(PCB& pcb : this->process_list){
+        cout<<"Running Process "<< pcb.name <<" for "<<pcb.burst_time<<" time units"<<endl;
+        pcb.arrival_time = waitTime;
+        waitTime += pcb.burst_time;
+        pcb.burst_time = waitTime;
+
+    }
+}
+
+void SchedulerPriority::print_results() {
+//    for(const PCB& pcb : this->process_list){
+//        // Accumulate total turnaround time
+//        this->tTime += pcb.burst_time;
+//
+//        // Accumulate total waiting time
+//        this->wTime += pcb.arrival_time;
+//
+//        // Print turnaround time and waiting time for the process
+//        cout << pcb.name << " turn-around time = " << pcb.burst_time << ", waiting time " << pcb.arrival_time << endl;
+//    }
+//
+//    // Calculate and print average turn-around time and average waiting time
+//    double avg_turnaround_time = this->tTime / this->process_list.size();
+//    double avg_waiting_time = this->wTime / this->process_list.size();
+//    cout << "Average turn-around time = " << avg_turnaround_time << ", Average waiting time = " << avg_waiting_time << endl;
+//}
+
+// Sort the process_list by id
+    std::sort(this->process_list.begin(), this->process_list.end(), compareId);
+
+    for(const PCB& pcb : this->process_list){
+        // Accumulate total turnaround time
+        this->tTime += pcb.burst_time;
+
+        // Accumulate total waiting time
+        this->wTime += pcb.arrival_time;
+
+        // Print turnaround time and waiting time for the process
+        cout << pcb.name << " turn-around time = " << pcb.burst_time << ", waiting time " << pcb.arrival_time << endl;
+    }
+
+    // Calculate and print average turn-around time and average waiting time
+    double avg_turnaround_time = this->tTime / this->process_list.size();
+    double avg_waiting_time = this->wTime / this->process_list.size();
+    cout << "Average turn-around time = " << avg_turnaround_time << ", Average waiting time = " << avg_waiting_time << endl;
+}
